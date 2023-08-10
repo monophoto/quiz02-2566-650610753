aeb0e8f95a5c29e449336d87604ba14fc6fe573a
monophoto <jetsadawut1013@gmail.com>
2023-08-10 15:24:38 +0700

    editPhoto

--- src/components/PostOwner.jsx
+++ src/components/PostOwner.jsx
@@ -3,7 +3,7 @@ export const PostOwner = (props) => {
     <div className="vstack gap-3">
       <div className="d-flex align-items-center gap-3">
         <img
-          src="/profileImages/Profile1.jpg"
+          src="/profileImages/Profile1.JPG"
           width="48"
           height="48"
           className="rounded-circle"
