a9f000691f9bbeba46e4006c90ba0834011ab3e6
monophoto <jetsadawut1013@gmail.com>
2023-08-10 15:21:30 +0700

    Qcompelete!

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,3 +1,5 @@
+import { Reply } from "./Reply";
+
 export const Comment = ({
   userImagePath,
   username,
@@ -5,10 +7,45 @@ export const Comment = ({
   likeNum,
   replies,
 }) => {
+  let likeNumOk = false;
+  if (likeNum > 0) likeNumOk = true;
   return (
     <div>
-      your code here...
+      <div className="d-flex gap-2 my-2">
+        <img
+          src={userImagePath}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <div
+          className="rounded rounded-3 p-2"
+          style={{ backgroundColor: "#3A3B3C" }}
+        >
+          <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+            {username}
+          </span>
+          <br />
+          <span style={{ color: "#E4E6EB" }}>{commentText}</span>
+          {likeNumOk && (
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+              <span style={{ color: "#B0B3B8" }}>{likeNum} คน</span>
+            </div>
+          )}
+        </div>
+      </div>
       {/* map-loop render Reply component here */}
+      {replies.map((reply) => (
+        <Reply
+          key={reply.username}
+          userImagePath={reply.userImagePath}
+          username={reply.username}
+          replyText={reply.replyText}
+          likeNum={reply.likeNum}
+        />
+      ))}
     </div>
   );
 };
--- src/components/PostOwner.jsx
+++ src/components/PostOwner.jsx
@@ -1,3 +1,26 @@
-export const PostOwner = ({}) => {
-  return <div>Your code here...</div>;
+export const PostOwner = (props) => {
+  return (
+    <div className="vstack gap-3">
+      <div className="d-flex align-items-center gap-3">
+        <img
+          src="/profileImages/Profile1.jpg"
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <span className="fw-semibold fs-5 text-white">
+          {props.fullName} {props.studentId}
+        </span>
+      </div>
+
+      <span className="text-white">{props.textPost}</span>
+
+      <div className="d-flex align-items-center gap-1">
+        <img src="/like.svg" width={20}></img>
+        <span style={{ color: "#B0B3B8" }}>{props.likeNum}</span>
+      </div>
+      <hr className="m-0 border" />
+    </div>
+  );
 };
